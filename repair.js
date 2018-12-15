3bd43fc825b87dfc627ca45eac8fbf5e237498f1
Bryan_lzh <390807154@qq.com>
2018-12-15 22:54:57 +0800

    Update repair.js
    增加了新说明

--- repair.js
+++ repair.js
@@ -2,7 +2,7 @@
 // @name         南理工教务处修复
 // @namespace    http://tampermonkey.net/
 // @version      0.1
-// @description  修复南京理工大学教务处教评无法弹出窗口的BUG
+// @description  修复南京理工大学教务处教评无法弹出窗口的BUG 以及成绩认定处 无法滚动的问题
 // @author       Bryan_lzh
 // @include      *://202.119.81.11*:9080/*
 // @grant        none
